8e6e2be6a9e2b3c9e17b191b02385d0e747ef205
NANI7674945099 <vantakulananaji@gmail.com>
2025-05-08 12:59:36 +0530

    component creation and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent,children:[
+    {path:'home',component:HomeComponent}
+  ]},
+  {path:'**',component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
