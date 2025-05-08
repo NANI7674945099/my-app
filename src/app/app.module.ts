e11de648ad05b754b5b303c757dd541430b3ed39
NANI7674945099 <vantakulananaji@gmail.com>
2025-05-08 19:16:37 +0530

    my website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { RectangleComponent } from './rectangle/rectangle.component';
+import { CircleComponent } from './circle/circle.component';
+import { BmiComponent } from './bmi/bmi.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { ErrorComponent } from './error/error.component';
     LoginComponent,
     HomeComponent,
     DashboardComponent,
-    ErrorComponent
+    ErrorComponent,
+    WelcomeComponent,
+    DatabindingComponent,
+    CalculatorComponent,
+    RectangleComponent,
+    CircleComponent,
+    BmiComponent
   ],
   imports: [
     BrowserModule,
